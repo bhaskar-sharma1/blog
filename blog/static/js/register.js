6d5a644545d813f7420a4a47afc899f2eb41c8fa
Bhaskar Mahor <bhaskar.mahor.07@gmail.com>
2023-05-13 23:58:18 +0530

    changing validation in jquery in progress

--- blog/static/js/register.js
+++ blog/static/js/register.js
@@ -1,13 +1,134 @@
 let csrf_token=$("input[name=csrfmiddlewaretoken]").val();
-let flags = {'email_flag':false,'username_flag':false,'password_flag':false}
-/* FORM HANDLE */
+// let flags = {'email_flag':false,'username_flag':false,'password_flag':false}
+username_valid = false;
+// Register Form Save Validation using jquery validation library
+$.validator.addMethod(
+    "validate_username",
+    function (value, element) {
+      const regex = /^[A-Za-z0-9_]+$/;
+      username_valid = regex.test(value) ? true : false;
+      return this.optional(element) || regex.test(value)
+  },
+  "Invalid user name"
+)
+$.validator.addMethod(
+    "is_username_exists",
+    function (value,element){
+        if(username_valid){
+            $.ajax({
+                headers : {
+                    'X-CSRFToken' : csrf_token
+                },
+                type:'POST',
+                url:'ajax/check-username/',
+                data : {
+                    username:value
+                },
+                dataType : 'JSON',
+                success : function(response){
+                    if(response.is_exists){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            })
+        }else{
+            return true;
+        }
+    },
+    "user name already exists"
+)
+$.validator.addMethod(
+    "validate_email",
+    function (value, element) {
+      // According to RBI guideline account number consists 9 to 18 digits
+      const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return this.optional(element) || regex.test(value)
+  },
+  "Invalid Email"
+);
+
+let register_form = $("#register-form").validate({
+    rules: {
+      username: {
+        required: {
+          depends: function () {
+            if ($("#username").val().trim()){
+                return false;
+            }else{
+                return true
+            }
+          },
+        },
+        validate_username:"Invalid user name",
+        is_username_exists:"user name already exists"
+      },
+      email: {
+        required: {
+          depends: function () {
+            if ($("#email").val().trim()){
+                return false;
+            }else{
+                return true
+            }
+          },
+        },
+        validate_email:"Invalid Email",
+      },
+      password: {
+        required: {
+          depends: function () {
+            if ($("#password").val().trim()){
+                return false;
+            }else{
+                return true
+            }
+          },
+        },
+      },
+      confirm_password: {
+        required: {
+          depends: function () {
+            if ($("#confirm-password").val().trim()){
+                return false;
+            }else{
+                return true
+            }
+          },
+        },
+        equalTo: "#password",
+      },
+    },
+    messages: {
+        username:{
+            required:"Please enter username"
+        },
+        email:{
+            required:"Please enter email"
+        },
+        confirm_password:{
+            equalTo:"Password did not match"
+        }
+    },
+    errorClass: "errorClass", //apply a css class for error if you have style for valid
+    validClass: "validClass", //apply a css class for error if you have style for error
+  
+    submitHandler: function (form) {
+      if ($("#register-form").valid()) {
+        form.submit();
+      }
+    },
+  });
+/******************
+// FORM HANDLE 
 $("#register-btn").on('click',(e)=>{
     e.preventDefault()
     if(flags.email_flag && flags.username_flag && flags.password_flag){
         $("#register-form").submit();
     }
 })
-/* USERNAME VALIDATION */ 
+// USERNAME VALIDATION
 $("#username").on('keyup',(e)=>{
     e.target.value = e.target.value.trim();
     let username = e.target.value.trim();
@@ -50,7 +171,7 @@ $("#username").on('keyup',(e)=>{
         }
     }
 })
-/* EMAIL VALIDATION */ 
+// EMAIL VALIDATION 
 $("#email").on('keyup',(e)=>{
     e.target.value=e.target.value.trim();
     let email = e.target.value.trim();
@@ -134,3 +255,6 @@ function hide_password_error(){
     $('.confirm-password-error').html('');
     $('#confirm-password').css({'border-color':'#4CBB17'})
 }
+
+
+***********************/
